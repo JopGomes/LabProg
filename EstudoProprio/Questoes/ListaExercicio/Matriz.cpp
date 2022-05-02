d92a6584921358ed6731a0ef4f65d4c2f669d024
Joseph Vieira <joseph25vieira@gmail.com>
2022-05-02 12:00:40 -0300

    MatrizLista

--- EstudoProprio/Questoes/ListaExercicio/Matriz.cpp
+++ EstudoProprio/Questoes/ListaExercicio/Matriz.cpp
@@ -8,18 +8,43 @@ using namespace std;
 // inverso da matriz, uma função det() que devolve o
 // determinante da matriz e uma função visualizar.
 
-class Matriz(){
+class Matriz{
     private:
-    int a,b,c,d;
+    double a,b,c,d;
     public:
-    Matriz(int aa,int bb int cc, int dd){
+    Matriz(double aa,double bb ,double cc, double dd){
         a=aa,b=bb,c=cc,d=dd;
     }
-    Matriz inversa(){
-        return Matriz()
+    double getA00(){
+        return a;
     }
-}
+    double getA01(){
+        return b;
+    }
+    double getA10(){
+        return c;
+    }
+    double getA11(){
+        return d;
+    }
+    double det(){
+        return getA00()*getA11() - getA10()*getA01();
+    }
+    Matriz inversa(){
+        double ai=getA11()/det();
+        double bi=-getA01()/det();
+        double ci=-getA10()/det();
+        double di=getA00()/det();
+        return Matriz(ai,bi,ci,di);
+    }
+    void imprimir(){
+        cout << getA00() <<" " <<getA01()<<" " << "\n" << getA10() <<" "<< getA11() << endl;
+    }
+};
 
 int main(){
-
+    Matriz a(2,0, 1,1);
+    cout << a.det()<<"\n";
+    a.imprimir();
+    a.inversa().imprimir();
 }
