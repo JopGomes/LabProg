21c6b9d1a7076e80670274ce3e600c8a8e48daef
Joseph Vieira <joseph25vieira@gmail.com>
2022-06-07 13:26:13 -0300

    Update

--- EstudoProprio/Questoes/VC2018/3.cpp
+++ EstudoProprio/Questoes/VC2018/3.cpp
@@ -3,28 +3,46 @@
 #include <algorithm>
 using namespace std;
 
-class Fib(){
+class Fib
+{
     vector<int> fibs;
     int pos;
+
 public:
-    Fib(){
-        pos=1;
+    Fib()
+    {
+        pos = 1;
         fibs.push_back(0);
         fibs.push_back(1);
     }
-    int seq(int val){
-        auto it= find(fibs.begin(),fibs.end(),val);
-        if(it!=fibs.end()){return (it - fibs.begin())++;}
-        else if(*it==val){return val.size();}
-        else{
-           int newComponent;
-           
+    int seq(int val)
+    {
+        int size = fibs.size();
+        auto it = find(fibs.begin(), fibs.end(), val);
+        if (it != fibs.end())
+        {
+            return distance(it, fibs.begin()) + 1;
+        }
+        else if (*it == val)
+        {
+            return size;
+        }
+        else
+        {
+            int newComponent;
+            while (newComponent != val)
+            {
+                auto it= fibs.rbegin();
+                newComponent = (*it) + (*it--);
+                fibs.push_back(newComponent);
+            }
+            return (size);
         }
     }
-}
+};
 
-
-
-int main(){
-
+int main()
+{
+    Fib a1;
+    cout << a1.seq(3);
 }
