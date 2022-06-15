3ee30c977344d63de5dc430fb6d471a9c63c0df0
Rafael Pontes <90840183+rpontestl@users.noreply.github.com>
2022-06-15 13:25:45 -0300

    Revert "Revert "feature: draw in terminal""
    This reverts commit c54ee0b78e01163e2214da1cbd07daa1f34d60a7.

--- TrabQueen/QueenTrab.cpp
+++ TrabQueen/QueenTrab.cpp
@@ -701,6 +701,7 @@ int main()
                         cout << "destX: " << destX << " destY: " << destY << endl;
                         if (p.Jogada(y, x, destY, destX, Pl, Op))
                         {   sound.play();
+                            p.imprimir();
                             if (p.termino())
                             {
                                 Image image;
@@ -733,6 +734,7 @@ int main()
                             else
                             {
                                 p.ComputerPlay(Pl);
+                                p.imprimir();
                             }
                             // p.imprimir();
                             if (p.termino())
@@ -772,6 +774,7 @@ int main()
         if (isGameBegin)
         {   background.setTexture(backGroundTexture);
             window.draw(background);
+            
             for (int i = 0; i < 8; i++)
             {
                 for (int j = 0; j < 8; j++)
