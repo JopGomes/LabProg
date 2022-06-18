4f1781f67f6b34ad5c57b41249393e71091c8858
JopGomes <joseph25vieira@gmail.com>
2022-06-18 13:09:25 -0300

    add path to SFML in compilasfml.sh

--- TrabQueen/compilasfml.sh
+++ TrabQueen/compilasfml.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 g++ -c QueenTrab.cpp
-g++ -c QueenTrab.cpp -Irafaelpontes/include
+g++ -c QueenTrab.cpp -ISFML/include
 g++ QueenTrab.o -o sfml-app -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system
 ./sfml-app
